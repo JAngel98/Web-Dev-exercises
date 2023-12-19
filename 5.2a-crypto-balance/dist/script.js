8383aa1eb683cc2d5a10a52056e72dc64bcfbe65
JAngel98 <jgarciarodriguez53@gmail.com>
2023-12-19 12:23:32 -0500

    Agregando funcion para obtener el saldo (primeros pasos)

---
+++ 5.2a-crypto-balance/dist/script.js
@@ -0,0 +1,31 @@
+// import axios from 'axios';
+
+const apiKeyElm = document.getElementById('api-key');
+const wAddressElm = document.getElementById('wallet-address');
+const balanceElm = document.getElementById('balance');
+const getInfoBtn = document.getElementById('get-info');
+
+async function getBalance() {
+    try {
+        await axios
+            .get(`https://api.covalenthq.com/v1/btc-mainnet/address/${wAddressElm.value}/balances_v2/?`, {
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                auth: {
+                    username: apiKeyElm.value,
+                },
+            })
+            .then((response) => {
+                const balance = response.data.data.items[0].balance;
+                balanceElm.innerText = balance + ' BTC';
+                console.log(response);
+            });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getInfoBtn.addEventListener('click', getBalance);
